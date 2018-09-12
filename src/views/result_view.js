d027389252c992a0096d2cd42dcb52d007290ff0
Filip Kuszenin <filipkuszenin@gmail.com>
2018-09-12 09:25:32 +0100

    Trying to add displayInstrumentList

--- src/views/result_view.js
+++ src/views/result_view.js
@@ -8,19 +8,47 @@ ResultView.prototype.bindEvents = function () {
 PubSub.subscribe('Instrument:details', (event) => {
   const instrumentObject = event.detail;
   console.log('ResultView has instrumentObject', instrumentObject);
-  this.display(instrumentObject);
+  this.displayDescription(instrumentObject);
+  this.displayInstrumentsList(instrumentObject);
 });
 };
 
-ResultView.prototype.display = function (instrumentObject) {
+ResultView.prototype.displayDescription = function (instrumentObject) {
 const descriptionList = document.querySelector('#result');
 console.log('ResultView has descriptionList', descriptionList);
 const paragraph = document.createElement('p');
 paragraph.textContent = `${instrumentObject.description}`;
 while (descriptionList.firstChild) {
   descriptionList.removeChild(descriptionList.firstChild);
-}
+};
 descriptionList.appendChild(paragraph);
+};
+
+ResultView.prototype.displayInstrumentsList = function (instrumentObject) {
+const instrumentsList = document.querySelector('#instruments-list');
+console.log('displayInstrumentsList has instrumentsList', instrumentsList);
+const olList = document.createElement('ol');
+olList.id = "list";
+instrumentsList.appendChild(olList);
+const allInstruments = instrumentObject.instruments;
+console.log('displayInstrumentsList has allInstruments', allInstruments)
+
+
+
+// const result = allInstruments.forEach((instrument) => `${instrument}`);
+// console.log(result);
+
+
+// let result = for (let i = 0; i < allInstruments.length; i++) {
+//   let instrument = allInstruments[i];
+//   `${instrument}`;
+// }
+// console.log('displayInstrumentsList has result:'result)
+// const result = for (instrument of allInstruments) {
+//   const ulList = document.createElement('ul');
+//   ulList.textContent = `${instrument}`;
+//   olList.appendChild(ulList);
+// }
 };
 
 module.exports =  ResultView;
